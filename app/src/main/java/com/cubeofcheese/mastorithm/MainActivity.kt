5469da316ae8c2aadd5121585c694b9033d0a7ec
CubeOfCheese <cubeofcheese@gmail.com>
2023-04-20 13:31:51 -0700

    initial commit

---
+++ app/src/main/java/com/cubeofcheese/mastorithm/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cubeofcheese.mastorithm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
